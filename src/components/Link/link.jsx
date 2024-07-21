3e762130b4f1961216762fce4a33fc280d0236d2
Jony2110 <146362462+Jony2110@users.noreply.github.com>
2024-07-21 15:07:28 +0500

    first commit

--- src/components/Link/link.jsx
+++ src/components/Link/link.jsx
@@ -7,11 +7,11 @@ function Link() {
   }
   return (
     <div className={styles.flex}>
-        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="../../../public/img/Google.svg" alt="" /></button>
-        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="../../../public/img/Insta.svg" alt="" /></button>
-        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="../../../public/img/telegram.svg" alt="" /></button>
-        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="../../../public/img/facebook.svg" alt="" /></button>
-        <button onClick={linklarr} className={styles.btn}><img  className={styles.img}src="../../../public/img/git.svg" alt="" /></button>
+        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="./img/Google.svg" alt="" /></button>
+        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="./img/Insta.svg" alt="" /></button>
+        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="./img/telegram.svg" alt="" /></button>
+        <button onClick={linklarr} className={styles.btn}><img className={styles.img} src="./img/facebook.svg" alt="" /></button>
+        <button onClick={linklarr} className={styles.btn}><img  className={styles.img}src="./img/git.svg" alt="" /></button>
         </div>
   )
 }
