168fcb619902a23ed5a1fe31c40edd2bd94737e2
Jony2110 <146362462+Jony2110@users.noreply.github.com>
2024-07-22 15:57:58 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,168 @@
-import { useState } from 'react'
-import styles from './App.module.css'
-import Header from './components/Header/header'
-import Input from './components/Input/input'
-import Dasturlar from './components/Dasturlar'
-import Link from './components/Link/link'
-import InputMini from './components/InputMin/Index'
-import InputBig from './components/InputBig'
-import Button from './components/Button/button'
+import { useState } from "react";
+import styles from "./App.module.css";
+import Header from "./components/Header/header";
+
+import Dasturlar from "./components/Dasturlar";
+import Link from "./components/Link/link";
+
 
 function App() {
-  const [fotoPush , setFotoPush] = useState('https://picsum.photos/200/300')
- 
+  const [fotoPush, setFotoPush] = useState("https://picsum.photos/200/300");
+  const [campany, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("null");
+  const [davlat, setDavlat] = useState("");
+  const [shahar, setShahar] = useState("");
+  const [joy, setJoy] = useState("");
+  const [soni, setSoni] = useState("null");
+  const [izoh, setIzoh] = useState("");
+  
+
   function handlePush() {
-   const fotoPush = setFotoPush(prompt("Rasm linkini kiriting"))
+    setFotoPush(prompt("Rasm linkini kiriting"));
   }
+  function handleCompany(event) {
+    setCompany(event.target.value);
+  }
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+  function handlePhone(event) {
+    setPhone(event.target.value);
+  }
+
+  function handleDavlat(event) {
+    setDavlat(event.target.value);
+  }
+  function handleShahar(event) {
+    setShahar(event.target.value);
+  }
+  function handleJoy(event) {
+    setJoy(event.target.value);
+  }
+  function handleSoni(event) {
+    setSoni(event.target.value);
+  }
+  function handleIzoh(event) {
+    setIzoh(event.target.value);
+  }
+  function handleForm(event) {
+    event.preventDefault();
+    let company = {
+      companyName: campany,
+      email: email,
+      phone: phone,
+      davlati: davlat,
+      shahari: shahar,
+      yashashJoyi: joy,
+      hodimlarSoni: soni,
+      izoh: izoh,
+    };
+    
+    const companyInfo = JSON.stringify(company);
+    localStorage.setItem("companyInfo", companyInfo);
+  }
+  
+
 
   return (
     <div>
-      <Header/>
-     <div className={styles.container}>
-     <h1 className={styles.h1}>Kompaniya ma`lumotlari</h1>
-     <p className={styles.p}>Kompaniya haqidagi ma’lumotlarni kiriting</p>
-     <div className= {styles.img}>
-     <img src={fotoPush} alt="Iltimos link yuboring" />
-     <button className= {styles.btn}  onClick={handlePush}>Yuklash</button>
-     </div>
-    
-    <Input  name="Kompaniya nomi" type ="text" inp = "company" />
-    <Input name="Email" type ="text" />
-    <Input name="Telefon raqam" phone="Uz +998" type ="number" />
-    <Dasturlar/>
-    <Link/>
-    <div className={styles.flexs}>
-    <InputMini name="Davlat" type ="text" />
-    <InputMini name="Shahar" type ="text"/>
+      <Header />
+      <div className={styles.container}>
+        <h1 className={styles.h1}>Kompaniya ma`lumotlari</h1>
+        <p className={styles.p}>Kompaniya haqidagi ma’lumotlarni kiriting</p>
+        <div className={styles.img}>
+          <img src={fotoPush} alt="Iltimos link yuboring" />
+          <button className={styles.btn} onClick={handlePush}>
+            Yuklash
+          </button>
+        </div>
+
+        <form> 
+          <div className={styles.box}>
+            <label htmlFor="">
+              Kompaniya nomi <span className={styles.zvezda}>*</span>
+            </label>
+            <input
+              onChange={handleCompany}
+              type="text"
+              placeholder="Kompaniya nomi"
+            />
+          </div>
+          <div className={styles.box}>
+            <label htmlFor="">
+              Email <span className={styles.zvezda}>*</span>
+            </label>
+            <input onChange={handleEmail} type="text" placeholder="Email" />
+          </div>
+          <div className={styles.box}>
+            <label htmlFor="">
+              Telefon raqam <span className={styles.zvezda}>*</span>
+            </label>
+            <input onChange={handlePhone} type="number" placeholder="Uz +998" />
+          </div>
+          <Dasturlar />
+          <Link />
+          <div className={styles.flexs}>
+            <div className={styles.miniBox}>
+              <label htmlFor="">
+                Davlat <span className={styles.zvezda}>*</span>
+              </label>
+              <input onChange={handleDavlat} type="text" placeholder="Davlat" />
+            </div>
+            <div className={styles.miniBox}>
+              <label htmlFor="">
+                Shahar <span className={styles.zvezda}>*</span>
+              </label>
+              <input onChange={handleShahar} type="text" placeholder="Shahar" />
+            </div>
+          </div>
+          <div className={styles.box}>
+            <label htmlFor="">
+              Yashash joyi <span className={styles.zvezda}>*</span>
+            </label>
+            <input
+              onChange={handleJoy}
+              type="text"
+              placeholder="Yashash joyi"
+            />
+          </div>
+          <div className={styles.box}>
+            <label htmlFor="">
+              Hodimlar soni <span className={styles.zvezda}>*</span>
+            </label>
+            <input
+              onChange={handleSoni}
+              type="number"
+              placeholder="Hodimlar soni"
+            />
+          </div>
+          <div className={styles.bigBox}>
+            <label htmlFor="">
+              Izoh <span className={styles.zvezda}>*</span>
+            </label>
+            <input
+              onChange={handleIzoh}
+              type="text"
+              placeholder="Kompaniya haqida izoh qoldiring"
+            />
+          </div>
+          <div className={styles.btnBack}>
+            <div className={styles.flexx}>
+              <button type="submit" className={styles.button1}>
+                ORTGA
+              </button>
+            </div>
+            <div className={styles.flexx}>
+              <button onClick={handleForm} className={styles.button1}>
+                KEYINGISI
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-    <Input name="Yashash joyi" type ="text" />
-    <Input name="Hodimlar soni" type ="number" />
-    <InputBig name="Izoh" type ="text"/>
-    <div className={styles.btnBack}>
-    <Button name = "ORTGA"/>
-    <Button name = "KEYINGISI"/>
-    
-    </div>
-     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
